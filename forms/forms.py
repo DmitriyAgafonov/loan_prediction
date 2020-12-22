5f497248576d03f3668bf8d479189df86f4e9f85
Sa4ras <nicesoup1989@protonmail.com>
2020-12-22 03:23:47 +0200

    changed DataRequired with InputRequired for best practice, fixed regex

--- forms/forms.py
+++ forms/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, HiddenField, SelectField, BooleanField
-from wtforms.validators import DataRequired, Regexp
+from wtforms.validators import InputRequired, Regexp
 
 class SignUpForm(FlaskForm):
 
@@ -8,50 +8,50 @@ class SignUpForm(FlaskForm):
 
     gender = SelectField("Gender: ",
                          choices=[('Male', 'Male'), ('Female', 'Female')],
-                         validators=[DataRequired("Please enter your gender.")])
+                         validators=[InputRequired("Please enter your gender.")])
 
     married = SelectField("Married: ",
                           choices=[('Yes', 'Yes'), ('No', 'No')],
-                          validators=[DataRequired("Please enter your marriage status.")])
+                          validators=[InputRequired("Please enter your marriage status.")])
 
     dependents = SelectField("Dependents: ",
                             choices=[('0', '0'), ('1', '1'), ('2', '2'), ('3+', '3+')],
-                            validators=[DataRequired("Please enter your dependents.")])
+                            validators=[InputRequired("Please enter your dependents.")])
 
     education = SelectField("Education: ",
                             choices=[('Graduate', 'Graduate'), ('Not Graduate', 'Not Graduate')],
-                            validators=[DataRequired("Please enter your graduation status.")])
+                            validators=[InputRequired("Please enter your graduation status.")])
 
     self_employed = SelectField("Self employment: ",
                                 choices=[('Yes', 'Yes'), ('No', 'No')],
-                                validators=[DataRequired("Please enter your employment status.")])
+                                validators=[InputRequired("Please enter your employment status.")])
 
     applicant_income = StringField("Applicant income: ",
-                                   validators=[DataRequired("Please enter your applicant income."),
-                                               Regexp("^[0-9]+$")])
+                                   validators=[InputRequired("Please enter your applicant income."),
+                                               Regexp(regex='^\d+.*\d+$', message='Input decimal number')])
 
     coapplicant_income = StringField("Co-applicant income: ",
-                                     validators=[DataRequired("Please enter your co-applicant income."),
-                                                 Regexp("^[0-9]+$")])
+                                     validators=[InputRequired("Please enter your co-applicant income."),
+                                                 Regexp(regex='^\d+.*\d+$', message='Input decimal number')])
 
     loan_amount = StringField("Loan amount (in thousands): ",
-                              validators=[DataRequired("Please enter your loan amount."),
-                                          Regexp("^[0-9]+$")])
+                              validators=[InputRequired("Please enter your loan amount."),
+                                          Regexp(regex='^\d+.*\d+$', message='Input decimal number')])
 
     loan_amount_term = StringField("Loan amount term (in months): ",
-                                   validators=[DataRequired("Please enter your loan amount."),
-                                               Regexp("^[0-9]+$")])
+                                   validators=[InputRequired("Please enter your loan amount."),
+                                               Regexp("^\d+$")])
 
     credit_history = SelectField("Did you have loan before?: ",
                                  choices=[('1', 'Yes'), ('0', 'No')],
-                                 validators=[DataRequired("Please enter your previous loan status.")])
+                                 validators=[InputRequired("Please enter your previous loan status.")])
 
     property_area = SelectField("Type of your property: ",
-                                 choices=[('Urban', 'Urban'), ('Rural', 'Rural'), ('Semiurban', 'Semiurban')],
-                                 validators=[DataRequired("Please enter type of your property.")])
+                                choices=[('Urban', 'Urban'), ('Rural', 'Rural'), ('Semiurban', 'Semiurban')],
+                                validators=[InputRequired("Please enter type of your property.")])
 
     agreement = BooleanField("I agree to provide my personal data to our Service",
-                             validators = [DataRequired("Please agree to continue.")])
+                             validators=[InputRequired("Please agree to continue.")])
 
     loan_status = HiddenField()
 
